dbc48ac5c0361b5a39c1a7a419a2f37acd0e155d
Eleila Bradford <eleila.bradford@gmail.com>
2022-02-26 22:36:27 -0700

    Problem 2 solved.

--- sldc.js
+++ sldc.js
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = 'This is a preliminary (or zeroth) step, in which the developers and project managers determine what the completed project should actually do, details of how it will accomplish that, and information on how users will be able to use it. If using agile development, this step will be repeated several times.'
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const design = 'This is the second step, in which the code functions are determined. Developers will work to design the code flow and process in this step.'
+const implementation = 'This is the third step, in which the design from the previous step is used to write the actual code for the project.'
+const tesetingAndIntegration = 'This is the fourth step, in which developers create and run methods to test their software. When the project is considered ready, the developers deliver it to the user or customer.'
+const maintain = 'This is the fifth step, in which developers respond to requests for fixes in missed errors, as well as design and integration of any future updates.'
